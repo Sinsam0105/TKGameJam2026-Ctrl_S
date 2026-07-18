b348b7f599b6245d807f29b495b826290ada008a
멍멍민 <pr620718@cu.ac.kr>
2026-07-18 21:42:17 +0900

    🚀 feat: FullScreenGlitchController
    - FullScreenGlitch 프리팹
    - 에디터에서 플레이 모드가 끝나면 글리치 강도 초기화
    
    - 지속 시간 or 무한으로 글리치 효과 재생
    - 램던으로 깜빡거리며 지속 시간 or 무한으로 글리치 효과 재생

---
+++ Assets/ControlS/Scripts/FullScreenGlitchController.cs
@@ -0,0 +1,180 @@
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class FullScreenGlitchController : MonoBehaviour
+{
+    [SerializeField] Material _material;
+
+    readonly int IntensityId = Shader.PropertyToID("_Intensity");
+
+    Coroutine _coPlay = null;
+    Coroutine _coPlayBurst = null;
+
+    void Awake()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        if (_material == null)
+        {
+            string name = transform.name.Replace("(Clone)", "").Trim();
+            _material = Resources.Load<Material>($"Material/{name}");
+        }
+
+        Debug.Log($"FullScreenGlitchController Init: {_material}");
+        Stop();
+
+        // test
+        PlayBurst(1f);
+    }
+
+    /// <summary>
+    /// 화면 전체에 글리치 효과를 무한 재생한다.
+    /// </summary>
+    /// <param name="intensity">글리치 강도 (0 ~ 1)</param>
+    public void Play(float intensity)
+    {
+        Play(-1, intensity);
+    }
+
+    /// <summary>
+    /// 몇 초 동안 화면 전체에 글리치 효과를 재생한다.
+    /// </summary>
+    /// <param name="duration">지속 시간</param>
+    /// <param name="intensity">글리치 강도 (0 ~ 1)</param>
+    /// <returns></returns>
+    public void Play(float duration, float intensity)
+    {
+        Stop();
+        _coPlay = StartCoroutine(CoPlay(duration, intensity));
+    }
+
+    /// <summary>
+    /// 화면 전체가 랜덤으로 깜빡거리며 글리치 효과를 무한 재생한다.
+    /// </summary>
+    /// <param name="maxIntensity"></param>
+    public void PlayBurst(float maxIntensity)
+    {
+        PlayBurst(-1, maxIntensity);
+    }
+
+    /// <summary>
+    /// 몇 초 동안 화면 전체가 랜덤으로 깜빡거리며 글리치 효과를 재생한다.
+    /// </summary>
+    /// <param name="duration">지속 시간</param>
+    /// <param name="maxIntensity">최대 글리치 강도 (0 ~ 1)</param>
+    public void PlayBurst(float duration, float maxIntensity)
+    {
+        Stop();
+        if (duration == -1)
+            _coPlayBurst = StartCoroutine(CoPlayBurst(maxIntensity));
+        else
+            _coPlayBurst = StartCoroutine(CoPlayBurst(duration, maxIntensity));
+    }
+
+    /// <summary>
+    /// 재생 중인 글리치 효과를 끈다.
+    /// </summary>
+    public void Stop()
+    {
+        if (_coPlay != null)
+        {
+            StopCoroutine(_coPlay);
+            _coPlay = null;
+        }
+
+        if (_coPlayBurst != null)
+        {
+            StopCoroutine(_coPlayBurst);
+            _coPlayBurst = null;
+        }
+    }
+
+    IEnumerator CoPlay(float duration, float intensity)
+    {
+        SetIntensity(intensity);
+        // TODO: To 팀장님. 나중에 글리치 효과음 넣어주세용 (지지지직)
+
+        if (duration != -1) // -1이면 멈추지 않고 무한 재생
+        {
+            yield return new WaitForSeconds(duration);
+            SetIntensity(0f);   // 정상 화면
+            _coPlay = null;
+        }
+    }
+
+    IEnumerator CoPlayBurst(float maxIntensity)
+    {
+        while (true)
+        {
+            float chance = Random.Range(0.1f, 0.5f);
+            float interval = Random.Range(0.03f, 0.2f);
+
+            if (Random.value <= chance)
+            {
+                float minIntensity = Mathf.Min(0.5f, maxIntensity);
+                SetIntensity(Random.Range(minIntensity, maxIntensity));
+            }
+            else
+                SetIntensity(0f);
+
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    IEnumerator CoPlayBurst(float duration, float maxIntensity)
+    {
+        int minCount = Mathf.Max(1, Mathf.FloorToInt(duration * 3f));
+        int maxCount = Mathf.Max(minCount + 1, Mathf.CeilToInt(duration * 6f));
+        
+        int burstCount = Random.Range(minCount, maxCount);
+        float interval = duration / burstCount;
+        
+        for (int i = 0; i < burstCount; i++)
+        {
+            float playDuration = interval * Random.Range(0.3f, 0.7f);
+            float stopDuration = interval - playDuration;
+            float minIntensity = Mathf.Min(0.5f, maxIntensity);
+
+            SetIntensity(Random.Range(minIntensity, maxIntensity));
+            yield return new WaitForSeconds(playDuration);
+
+            SetIntensity(0f);
+            yield return new WaitForSeconds(stopDuration);
+        }
+
+        SetIntensity(0f);
+        _coPlayBurst = null;
+    }
+
+    /// <summary>
+    /// 0(정상 화면) ~ 1(글리치 강도 최대)
+    /// </summary>
+    /// <param name="intensity"></param>
+    void SetIntensity(float intensity)
+    {
+        _material.SetFloat(IntensityId, Mathf.Clamp01(intensity));
+    }
+
+#if UNITY_EDITOR
+    void OnEnable()
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+    void OnDisable()
+    {
+        EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+    }
+
+    // 에디터에서 플레이 모드가 끝나면 글리치 강도 초기화
+    void OnPlayModeStateChanged(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingPlayMode)
+            SetIntensity(0f);
+    }
+#endif
+}
