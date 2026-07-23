ad5bf536b25c1624f9bee13963cb3230c92d650e
멍멍민 <pr620718@cu.ac.kr>
2026-07-23 17:00:38 +0900

    🚀 feat: 메인화면 초기 UI
    -  메인화면 디자인
    - TODO: 설정 팝업창

---
+++ Assets/ControlS/Scripts/UI/MainSceneUI.cs
@@ -0,0 +1,49 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainSceneUI : MonoBehaviour
+{
+    [SerializeField] private Button _startButton;
+    [SerializeField] private Button _optionsButton;
+    [SerializeField] private Button _exitButton;
+
+    [SerializeField] private Transform _optionsUI;
+
+    void Awake()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        _startButton ??= transform.GetComponentsInChildren<Button>(true).FirstOrDefault(t => t.name == "StartButton");
+        _optionsButton ??= transform.GetComponentsInChildren<Button>(true).FirstOrDefault(t => t.name == "OptionsButton");
+        _exitButton ??= transform.GetComponentsInChildren<Button>(true).FirstOrDefault(t => t.name == "ExitButton");
+        _optionsUI ??= transform.GetComponentsInChildren<Transform>(true).FirstOrDefault(t => t.name == "OptionsUI");
+
+        _startButton.onClick.AddListener(OnStartButtonClicked);
+        _optionsButton.onClick.AddListener(OnOptionsButtonClicked);
+        _exitButton.onClick.AddListener(OnExitButtonClicked);
+        _optionsUI.gameObject.SetActive(false);
+    }
+
+    void OnStartButtonClicked()
+    {
+        Debug.Log("OnStartButtonClicked");
+        //SceneManager.LoadScene("GameScene");
+    }
+
+    void OnOptionsButtonClicked()
+    {
+        Debug.Log("OnOptionsButtonClicked");
+        _optionsUI.gameObject.SetActive(true);
+    }
+
+    void OnExitButtonClicked()
+    {
+        Debug.Log("OnExitButtonClicked");
+        Application.Quit();
+    }
+}
